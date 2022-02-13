fc68b38514d0c78616aae5d9fb7e42fc2b50ed0c
Bomby <77249735+bombyy@users.noreply.github.com>
2022-02-13 03:21:50 +0100

    Add files via upload

---
+++ posh-install.sh
@@ -0,0 +1,16 @@
+cd ~
+sudo wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/posh-linux-amd64 -O /usr/local/bin/oh-my-posh
+sudo chmod +x /usr/local/bin/oh-my-posh
+
+mkdir ~/.poshthemes
+wget https://github.com/JanDeDobbeleer/oh-my-posh/releases/latest/download/themes.zip -O ~/.poshthemes/themes.zip
+unzip ~/.poshthemes/themes.zip -d ~/.poshthemes
+chmod u+rw ~/.poshthemes/*.json
+rm ~/.poshthemes/themes.zip
+
+cp ~/oh-my-posh-setup/bubbles_nc.omp.json ~/.poshthemes/
+
+sudo echo 'eval "$(oh-my-posh --init --shell bash --config ~/.poshthemes/bubbles_nc.omp.json)"' >> ./.bashrc
+
+. .bashrc
+
